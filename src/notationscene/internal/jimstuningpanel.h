2b6eae200fa62d31911d7dff9799efd5a64d3875
Jim Plamondon <JimLPlamondon@GMail.com>
2026-08-25 10:24:47 +0700

    Preserve pinned formatter baseline after merge
    Signed-off-by: Jim Plamondon <JimLPlamondon@GMail.com>

--- src/notationscene/internal/jimstuningpanel.h
+++ src/notationscene/internal/jimstuningpanel.h
@@ -49,8 +49,8 @@ class JimsTuningPanel : public QWidget, public muse::async::Asyncable
     Q_OBJECT
 
 public:
-    JimsTuningPanel(mu::engraving::Score* score, std::function<void()> refreshView, muse::async::Notification scoreChanged,
-                    QWidget* parent = nullptr);
+    JimsTuningPanel(mu::engraving::Score* score,
+                    std::function<void()> refreshView, muse::async::Notification scoreChanged, QWidget* parent = nullptr);
     ~JimsTuningPanel() override;
 
 private:
